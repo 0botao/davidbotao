1eecf47dbc6469508f1a4892b0d74b6d8611a48b
0botao <contatodavidbotao@gmail.com>
2024-04-30 16:19:53 -0300

    exercicio 2

--- exercicios/exercicio2.java
+++ exercicios/exercicio2.java
@@ -1,4 +1,7 @@
 package exercicios;
+import java.util.Locale;
+
+import java.util.Scanner;
 
 public class exercicio2 {
     public static void main(String[] args) {
@@ -7,7 +10,19 @@ public class exercicio2 {
         //Fórmula da área: area = π . raio2
         //Considere o valor de π = 3.14159
 
-        
+        double x, raio ;
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        System.out.println(" digite o  raio de um círculo ");
+        x = sc.nextDouble();
+
+        raio = Math.pow(x, 2) * 3.14159;
+
+        System.out.printf(" Área do circulo é de %.4f%n" , raio);
+
+        sc.close();
+
     }
     
 }
