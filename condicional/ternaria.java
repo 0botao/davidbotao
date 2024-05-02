b52e8aeeea551dc3926676068747f3f128a0eb87
0botao <contatodavidbotao@gmail.com>
2024-05-02 15:45:33 -0300

    feito

--- condicional/ternaria.java
+++ condicional/ternaria.java
@@ -3,7 +3,9 @@ package condicional;
 public class ternaria {
     public static void main(String[] args) {
         double preco = 34.5;
-        double desconto ""
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05 ;
+
+        System.out.println("o valor com desconto é " + (preco - desconto));
     }
     
 }
